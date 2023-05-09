b7cfc14b76b36ef114c1e9febcf599dee7e33799
Tinayaan <Tina.banaee.business@gmail.com>
2023-05-09 20:58:35 +0330

    Add out initial land page which is fucking cooool

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.image-container img');
+console.log(images[0]);
+let currentImageIndex = 0;
+
+function showNextImage() {
+  // Hide the current image
+  images[currentImageIndex].style.opacity = 0;
+  
+  // Increment the index and wrap around if necessary
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  
+  // Show the next image
+  images[currentImageIndex].style.opacity = 1;
+}
+
+// Show the first image immediately
+images[currentImageIndex].style.opacity = 1;
+
+
+// Show the next image every 5 seconds
+setInterval(showNextImage, 5000);
+  });
