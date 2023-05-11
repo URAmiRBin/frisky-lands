c8d741b395b80d0f970e590116dc7631414296cb
AmiR Bin <ur.amirbin@gmail.com>
2023-05-11 16:49:29 +0330

    Add mobile support

--- main.js
+++ main.js
@@ -3,22 +3,24 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll('.image-container img');
-console.log(images[0]);
 let currentImageIndex = 0;
 
 function showNextImage() {
   // Hide the current image
   images[currentImageIndex].style.opacity = 0;
+  images[currentImageIndex + 3].style.opacity = 0;
   
   // Increment the index and wrap around if necessary
-  currentImageIndex = (currentImageIndex + 1) % images.length;
+  currentImageIndex = (currentImageIndex + 1) % (images.length / 2);
   
   // Show the next image
   images[currentImageIndex].style.opacity = 1;
+  images[currentImageIndex + 3].style.opacity = 1;
 }
 
 // Show the first image immediately
 images[currentImageIndex].style.opacity = 1;
+images[currentImageIndex + 3].style.opacity = 1;
 
 
 // Show the next image every 5 seconds
